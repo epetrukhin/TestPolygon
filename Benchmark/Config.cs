7b198936270dd6b15841c98f11bcd0cc60503065
Eduard Petrukhin <e.petruhin@almara.org>
2017-05-05 17:00:59 +0300

    Refactor

---
+++ Benchmark/Config.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+
+namespace Benchmark
+{
+    public sealed class Config : ManualConfig
+    {
+        public Config()
+        {
+//                Add(MemoryDiagnoser.Default);
+//                Add(StatisticColumn.P95);
+
+            Add(
+                Job.LegacyJitX86
+                    .WithLaunchCount(1)
+                    .WithWarmupCount(3)
+                    .WithTargetCount(5),
+                Job.LegacyJitX64
+                    .WithLaunchCount(1)
+                    .WithWarmupCount(3)
+                    .WithTargetCount(5),
+                Job.RyuJitX64
+                    .WithLaunchCount(1)
+                    .WithWarmupCount(3)
+                    .WithTargetCount(5));
+        }
+    }
+}
