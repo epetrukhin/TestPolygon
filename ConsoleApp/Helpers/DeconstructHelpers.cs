59a8dee96936d143ff1f8e14ca77ddfe88e62be4
Eduard Petrukhin <e.petruhin@almara.org>
2018-07-27 11:03:47 +0300

    NuGet packages update

--- ConsoleApp/Helpers/DeconstructHelpers.cs
+++ ConsoleApp/Helpers/DeconstructHelpers.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
 
 namespace ConsoleApp.Helpers
 {
@@ -8,6 +11,15 @@ namespace ConsoleApp.Helpers
         {
             key = kvp.Key;
             value = kvp.Value;
+        }
+
+        public static void Deconstruct<TKey, TElement>([NotNull] this IGrouping<TKey, TElement> group, out TKey key, [NotNull] out IEnumerable<TElement> elements)
+        {
+            if (group == null)
+                throw new ArgumentNullException(nameof(group));
+
+            key = group.Key;
+            elements = group;
         }
     }
 }
